d54677007f3e9c8a2a8149eeeb777e1b81640fd8
czarny994 <37272023+czarny994@users.noreply.github.com>
2021-01-29 15:28:38 +0100

    Create vagrant_install.sh

---
+++ vagrant_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+echo "*************** Update system ***************"
+sudo yum -y update
+echo "*************** Install VirtualBox ***************"
+sudo yum -y install kernel-devel kernel-devel-$(uname -r) kernel-headers kernel-headers-$(uname -r) make patch gcc
+sudo wget https://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo -P /etc/yum.repos.d
+sudo yum -y install VirtualBox-5.2
+
+if sudo systemctl status vboxdrv | grep 'active (exited)';
+then
+    echo "****** Virtualbox was installed ******"
+else
+    echo "****** Virtualbox was not installed ******"
+    exit 1
+fi
+
+echo "*************** Install Vagrant ***************"
+sudo yum -y install https://releases.hashicorp.com/vagrant/2.2.6/vagrant_2.2.6_x86_64.rpm
+
+if vagrant --version | grep "Vagrant 2.2.6";
+then
+    echo "****** Vagrant was installed ******"
+else
+    echo "****** Vagrant was not installed ******"
+    exit 1
+fi
