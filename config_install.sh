03571e4207290b288d6e38c63731155709fc1149
czarny994 <37272023+czarny994@users.noreply.github.com>
2021-01-30 18:44:49 +0100

    Create config_install.sh

---
+++ config_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "###### Update System ######"
+sudo yum -y update
+echo "###### Nano install ######"
+sudo yum -y install nano
+echo "###### Git install ######"
+sudo yum -y install git
+echo "###### Net-tools install ######"
+sudo yum -y install net-tools
