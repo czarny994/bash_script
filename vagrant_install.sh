6113a6cc9f1b0b566f7e2fb6f62954aea5dd1841
czarny994 <37272023+czarny994@users.noreply.github.com>
2021-01-30 18:49:39 +0100

    Update vagrant_install.sh

--- vagrant_install.sh
+++ vagrant_install.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 
-echo "*************** Update system ***************"
+echo "############*** Update system ############***"
 sudo yum -y update
-echo "*************** Install VirtualBox ***************"
+echo "############*** Install VirtualBox ############***"
 sudo yum -y install kernel-devel kernel-devel-$(uname -r) kernel-headers kernel-headers-$(uname -r) make patch gcc
 sudo wget https://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo -P /etc/yum.repos.d
 sudo yum -y install VirtualBox-5.2
 
 if sudo systemctl status vboxdrv | grep 'active (exited)';
 then
-    echo "****** Virtualbox was installed ******"
+    echo "###### Virtualbox was installed ######"
 else
-    echo "****** Virtualbox was not installed ******"
+    echo "###### Virtualbox was not installed ######"
     exit 1
 fi
 
-echo "*************** Install Vagrant ***************"
+echo "############*** Install Vagrant ############***"
 sudo yum -y install https://releases.hashicorp.com/vagrant/2.2.6/vagrant_2.2.6_x86_64.rpm
 
 if vagrant --version | grep "Vagrant 2.2.6";
 then
-    echo "****** Vagrant was installed ******"
+    echo "###### Vagrant was installed ######"
 else
-    echo "****** Vagrant was not installed ******"
+    echo "###### Vagrant was not installed ######"
     exit 1
 fi
 
-echo "*************** Install vagrant-vbguest ***************"
+echo "############*** Install vagrant-vbguest ############***"
 vagrant plugin uninstall vagrant-vbguest || true
 vagrant plugin install vagrant-vbguest --plugin-version 0.21
 if [ $? -eq 0 ]; then
@@ -36,3 +36,6 @@ else
     echo "Plugin was not installed"
     exit 1
 fi
+
+echo "###### Clear ######"
+sudo yum clean all
